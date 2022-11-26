7ec69277115af0ad39a3c00dcbe7eff4141e6b58
NSRBSG <nsrbsg@gmail.com>
2022-11-27 02:16:15 +0900

    Home 페이지 틀 구현

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from 'react';
+import style from '../../style/Home.module.css';
+import Post from '../../components/Post.js';
+
 const Home = () => {
+  const [posts, setPosts] = useState([
+    {
+      name: 'string',
+      address: 'string',
+      startDate: '2022/11/01',
+      endDate: '2022/11/30',
+    },
+    {
+      name: 'string',
+      address: 'string',
+      startDate: '2022/11/01',
+      endDate: '2022/11/30',
+    },
+    {
+      name: 'string',
+      address: 'string',
+      startDate: '2022/11/01',
+      endDate: '2022/11/30',
+    },
+  ]);
+
+  const [countPosts, setCountPosts] = useState(0);
+
+  useEffect(() => {
+    const getPosts = () => {};
+  }, []);
+
   return (
     <>
-      <h1>hello</h1>
+      <div className={style.container}>
+        <div className={style.wrapper}>
+          {posts?.map((post, index) => (
+            <Post key={index} post={post} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
