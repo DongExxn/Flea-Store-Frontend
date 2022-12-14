7f06856a5b1a75a11b217f746005876122f0a655
DongExxn <pobi555@naver.com>
2022-12-15 03:11:57 +0900

    목록페이지 및 상세페이지 기능구현

--- src/pages/App.js
+++ src/pages/App.js
@@ -12,9 +12,8 @@ import MyPage from './Mypage/MyPage';
 import Calender from './Calender';
 import Favorite from './Favorite';
 import Manage from './Manage';
-import MarketEnroll2 from './MarketList/MarketEnroll2';
-import MarketList from './MarketList/MarketList';
-import Market from './Detail/Market';
+import MarketList from './MarketList/MarketList'; //목록페이지
+import Market from './Detail/Market'; //상세조회페이지
 
 function App() {
   const [name, setName] = useState('');
@@ -57,59 +56,42 @@ function App() {
       <BrowserRouter>
         <NavigationBar name={name} />
         <Routes>
-          <Route path="/" exact element={<Home />}></Route>
-          <Route path="/login" exact element={<Login setName={setName} setUser={setUser} />}></Route>
-          <Route path="/description" exact element={<Description />}></Route>
+          <Route
+            path="/"
+            exact
+            element={
+              user.authority === 'ROLE_ADMIN' ? <Manage /> : <Home />
+            }
+          ></Route>
+          <Route
+            path="/login"
+            exact
+            element={<Login setName={setName} setUser={setUser} />}
+          ></Route>
+          <Route
+            path="/description"
+            exact
+            element={<Description />}
+          ></Route>
           <Route path="/Register" exact element={<Register />}></Route>
-          <Route path="/MyPage" exact element={<MyPage user={user} settingName={setName} setUser={setUser} />}></Route>
-          <Route path="/Calender" exact element={<Calender />}></Route>
-          <Route path="/Favorite" exact element={user.name ? <Favorite /> : <Navigate to={'/login'} />}></Route>
-          <Route path="/Manage" exact element={<Manage />}></Route>
           <Route path="/store" exact element={<MarketList />}></Route>
           <Route path="/detail" exact element={<Market />}></Route>
+          <Route
+            path="/MyPage"
+            exact
+            element={
+              <MyPage user={user} settingName={setName} setUser={setUser} />
+            }
+          ></Route>
+          <Route path="/Calender" exact element={<Calender />}></Route>
+          <Route
+            path="/Favorite"
+            exact
+            element={
+              name ? <Favorite token={token} /> : <Navigate to={'/login'} />
+            }
+          ></Route>
         </Routes>
-        {loading ? (
-          <h1>hi</h1>
-        ) : (
-          <>
-            <NavigationBar name={name} />
-            <Routes>
-              <Route
-                path="/"
-                exact
-                element={
-                  user.authority === 'ROLE_ADMIN' ? <Manage /> : <Home />
-                }
-              ></Route>
-              <Route
-                path="/login"
-                exact
-                element={<Login setName={setName} setUser={setUser} />}
-              ></Route>
-              <Route
-                path="/description"
-                exact
-                element={<Description />}
-              ></Route>
-              <Route path="/Register" exact element={<Register />}></Route>
-              <Route
-                path="/MyPage"
-                exact
-                element={
-                  <MyPage user={user} settingName={setName} setUser={setUser} />
-                }
-              ></Route>
-              <Route path="/Calender" exact element={<Calender />}></Route>
-              <Route
-                path="/Favorite"
-                exact
-                element={
-                  name ? <Favorite token={token} /> : <Navigate to={'/login'} />
-                }
-              ></Route>
-            </Routes>
-          </>
-        )}
       </BrowserRouter>
     </>
   );
