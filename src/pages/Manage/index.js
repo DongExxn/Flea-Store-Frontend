dc702db73e6b2f682b5358c8ce5a000f5065376c
NSRBSG <nsrbsg@gmail.com>
2022-12-14 21:34:36 +0900

    달력 구현, 매니지 구현

--- src/pages/Manage/index.js
+++ src/pages/Manage/index.js
@@ -1,45 +1,91 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import style from '../../style/Manage.module.css';
+import LevelUpModal from '../Mypage/levelUpModal';
 
 const Manage = () => {
-  const [requestUsers, setRequestUsers] = useState([
-    {
-      email: 'string',
-      name: '김명식',
-      nickName: '수산물시장',
-      PhoneNumber: '010-1234-5678',
-      authority: 0,
-    },
-    {
-      email: 'string',
-      name: '윤은혜',
-      nickName: '핸드폰팔이',
-      PhoneNumber: '010-1234-5678',
-      authority: 0,
-    },
-    {
-      email: 'string',
-      name: '손미정',
-      nickName: '헌옷판매',
-      PhoneNumber: '010-1234-5678',
-      authority: 0,
-    },
-  ]);
+  const [requestUsers, setRequestUsers] = useState([]);
+  const [show, setShow] = useState(false);
+  const [detail, setDetail] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/auth/permission-list')
+      .then((result) => {
+        setRequestUsers(result.data.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const onClickDetail = (event) => {
+    const {
+      target: { name },
+    } = event;
+    axios
+      .post(`http://localhost:8080/auth/permission-detail?requestId=${name}`)
+      .then((result) => {
+        setShow(true);
+        setDetail(result.data.data);
+      })
+      .catch((error) => alert('에러'));
+  };
+
+  const onClickAccept = (event) => {
+    const {
+      target: { name },
+    } = event;
+    axios
+      .post(`http://localhost:8080/auth/permission-accept?requestId=${name}`)
+      .then((result) => {
+        window.location.reload(true);
+        alert('등록 완료');
+      })
+      .catch((error) => alert('에러'));
+  };
+
+  const onClickDecline = (event) => {
+    const {
+      target: { name },
+    } = event;
+    axios
+      .post(`http://localhost:8080/auth/permission-decline?requestId=${name}`)
+      .then((result) => {
+        alert('취소 완료');
+        window.location.reload(true);
+      })
+      .catch((error) => alert('에러'));
+  };
 
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
+        {show && <LevelUpModal detail={detail} setShow={setShow} />}
         {requestUsers?.map((user) => (
           <div className={style.requestBox}>
             <div className={style.requestInfo}>
               <span>{`성함 : ${user.name}`}</span>
               <span>{`이메일 : ${user.email}`}</span>
-              <span>{`전화번호 : ${user.PhoneNumber}`}</span>
+              <span>{`전화번호 : ${user.phoneNumber}`}</span>
             </div>
             <div className={style.requestBtnBox}>
-              <input type="button" value="디테일" />
-              <input type="button" value="승인" />
-              <input type="button" value="취소" />
+              <input
+                onClick={onClickDetail}
+                name={user.requestId}
+                type="button"
+                value="디테일"
+              />
+              <input
+                onClick={onClickAccept}
+                name={user.requestId}
+                type="button"
+                value="승인"
+              />
+              <input
+                onClick={onClickDecline}
+                name={user.requestId}
+                type="button"
+                value="취소"
+              />
             </div>
           </div>
         ))}
